cfefdce8159d47cb2d0dd4594761cba425d025c8
cynthia <cynthiakagz@gmail.com>
2023-03-25 23:34:45 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+let z=a+b
+function add() {
+return z;
+}
+
+let x=a-b
+function subtract() {
+  return x;
+}
+let y=a*b
+function multiply() {
+  return y;
+}
+let c=a/b
+function divide() {
+return c;
+}
+function add(a,b) {
+  return a+b;
+}
+function subtract(a,b) {
+  return a-b;
+}
+function multiply(a,b) {
+  return a*b;
+}
+function divide(a,b) {
+  return a/b;
+}
+function increment(a) {
+  return a+1;
+}
+function decrement(a) {
+  return a-1;
+}
+function makeInt(a,toString()) {
+  return a;
+}
+function makeInt('0x2328'()) {
+  return 0;
+}
